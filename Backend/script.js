a6d858c10ac967b019514db319e5c1a0cf7c8e0b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2024-01-24 01:13:00 +0100

    ajout page de connexion et sass

--- Backend/script.js
+++ Backend/script.js
@@ -1,15 +1,31 @@
 
+// Je charge le DOM avant d'executer le script
+document.addEventListener("DOMContentLoaded", (event) => {
+
+
+// Je récupère l'élément HTML qui a la classe "gallery"
 const galleryElement = document.querySelector(".gallery");
 
-fetch("http://localhost:5678/api/works")
-    .then(response => {
-        return response.json();
-    }) .then((users) => {
-        console.log(users);
-        for (const user of users) {
-            const figureElement = document.createElement("figure");
-            figureElement.innerText = user.title;
-            galleryElement.appendChild(figureElement);
-        }
 
+fetch("http://localhost:5678/api/works") // Je fais une requête HTTP pour récupérer les données de l'API
+    .then(response => { // Je récupère la réponse de l'API
+        return response.json(); // Je convertis la réponse en JSON
+    }) .then((works) => { // Je récupère les données JSON
+        console.log(works); 
+        for (const work of works) { // Je parcours les données 
+
+            const galleryElement = document.createElement("figure"); 
+            galleryElement.classList.add("gallery-item");
+            document.querySelector(".gallery").appendChild(galleryElement);
+
+            const imageElement = document.createElement("img");
+            imageElement.src = work.imageUrl;
+            galleryElement.appendChild(imageElement);
+
+            const titleElement = document.createElement("figcaption");
+            titleElement.innerText = work.title;
+            galleryElement.appendChild(titleElement);
+         
+        }
     });
+});
